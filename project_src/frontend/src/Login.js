1717f0f9d99554a83169392661ddf8997f5500ef
Piotr Augustowski <peteros9999@gmail.com>
2020-11-25 03:50:29 +0100

    Added login and registration components

---
+++ project_src/frontend/src/Login.js
@@ -0,0 +1,62 @@
+import {useState} from 'react';
+import {Grid, TextField, Button, Tab, Typography} from "@material-ui/core"
+import {useHistory, Link} from "react-router-dom";
+import {roles} from "./Roles"
+import {roleContext} from "./RoleContext"
+
+export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    let userRole;
+
+    function handleLogin(event) {
+        event.preventDefault();
+        fetch(`/api/account?email=${email}&password=${password}`)
+            .then(
+                response => {
+                    if(response.ok) {
+                        response.json()
+                            .then(account =>{
+                               userRole = account.hasOwnProperty("role") ? account["role"] : "user";
+                            })
+                    }
+                    else {
+                        alert("Incorrect credentials");
+                    }
+            })
+    }
+
+    return(
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            >
+            <div>
+                <form onSubmit={handleLogin}>
+                    <Grid item xs={12}>
+                        <TextField id="email-input" label="email" onInput={ e =>setEmail(e.target.value)} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField id="password-input" label="password" onInput={ e =>setPassword(e.target.value)} />
+                    </Grid>
+                    <roleContext.Provider value={userRole}>
+                        <Grid item xs={12}>
+                            <Button type="submit" component={Link} to={'/'} variant="contained" size="large" color="primary">Login</Button>
+                        </Grid>
+                    </roleContext.Provider>
+                    <roleContext.Provider value={roles.USER}>
+                        <Grid item xs={12}>
+                            <Typography>
+                                <Tab label='Register'  to='/register' component={Link} />
+                            </Typography>
+                        </Grid>
+                    </roleContext.Provider>
+                </form>
+            </div>
+        </Grid>
+    )
+}
