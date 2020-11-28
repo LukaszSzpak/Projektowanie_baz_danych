984394d00b135387cad84f1a377d6fbe0271bf07
Piotr Augustowski <peteros9999@gmail.com>
2020-11-28 19:53:05 +0100

    Added separate folder for login-registration, added email and role state to app.js and accordingly moddified login and registration

--- libraryDB/frontend/src/App.js
+++ libraryDB/frontend/src/App.js
@@ -1,16 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Route, Switch } from 'react-router-dom';
-import Login from './Login'
+import Login from './login-registration/Login'
 import Register from './Register'
 import {roleContext} from "./RoleContext"
 
 export default function App() {
 
+  const [role, setRole] = useState('');
+  const [email, setEmail] = useState('');
+
+
   return (
     <div className="App">
       <Switch>
           <Route path='/login'>
-            <Login/>
+            <Login setRole={setRole} setEmail={setEmail}/>
           </Route>
           <Route path='/register'>
             <Register/>
