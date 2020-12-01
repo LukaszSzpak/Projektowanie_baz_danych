d352ccabd511bf9493e0fd649ac3abd3cabf9e39
Łukasz Szpak <alukasz1@interia.pl>
2020-12-01 19:14:33 +0100

    Json fix

--- libraryDB/src/main/java/com/library/libraryDB/controllers/LoanController.java
+++ libraryDB/src/main/java/com/library/libraryDB/controllers/LoanController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sun.plugin.javascript.navig.Link;
 
 import java.util.List;
 
