c904adbf3ddff079f48c582d4b7088fe3e5bf7d4
Łukasz Szpak <alukasz1@interia.pl>
2021-01-18 18:37:35 +0100

    Revert "Copy NoSQL project for SQL adjustment"
    This reverts commit 8b2d2c96

--- libraryDB/frontend/src/Login.js
+++ libraryDB/frontend/src/Login.js
@@ -26,6 +26,7 @@ export default function Login(props) {
                                 setEmail(account["email"]);
                                 setId(account["id"]);
                                 history.push('/home');
+                                //history.push(`/${userRole}`);
                             })
                     }
                     else {
